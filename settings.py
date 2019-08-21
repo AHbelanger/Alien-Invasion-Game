2e7e0ae141941834a3b8c434d55732bc28e4e454
Ahbelanger <hbelanger.antoine@gmail.com>
2019-08-21 18:21:32 -0400

    ship moves/shoots

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 500
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 8
+
+        # Bullet settings
+        self.bullet_speed = 5.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
