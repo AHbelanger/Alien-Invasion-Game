0029ebe11caec43f681e0a435a74603dcacdecbd
Ahbelanger <hbelanger.antoine@gmail.com>
2019-08-21 23:00:05 -0400

    allow collisions between bullets/aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -84,6 +85,21 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        # Check for collisions between bullets and aliens
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
@@ -107,6 +123,17 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
--- settings.py
+++ settings.py
@@ -9,14 +9,22 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 8
+        self.ship_speed = 10.0
 
         # Bullet settings
-        self.bullet_speed = 5.0
-        self.bullet_width = 3
+        self.bullet_speed = 7.0
+        self.bullet_width = 6
         self.bullet_height = 15
         self.bullet_color = (230, 0, 0)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 rep. right and -1 rep. left
+        self.fleet_direction = 1
+
+
 
 
 
